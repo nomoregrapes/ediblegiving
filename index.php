3343794c47ccddac05fd487b18981380341078cb
Gregory <info@nomoregrapes.com>
2015-03-02 15:13:48 +0000

    Add index/intro page.

---
+++ index.php
@@ -0,0 +1,64 @@
+<html>
+<head>
+	<title>Food Giving</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" />
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css" />
+    <link href="lib/bootstrap-cover.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+    <div class="site-wrapper">
+
+      <div class="site-wrapper-inner">
+
+        <div class="cover-container">
+
+          <div class="masthead clearfix">
+            <div class="inner">
+              <h3 class="masthead-brand">Edible Giving</h3>
+              <!--
+              <nav>
+                <ul class="nav masthead-nav">
+                  <li class="active"><a href="#">Home</a></li>
+                  <li><a href="#">Features</a></li>
+                  <li><a href="#">Contact</a></li>
+                </ul>
+              </nav>
+              -->
+            </div>
+          </div>
+
+          <div class="inner cover">
+            <h1 class="cover-heading">Edible Giving.</h1>
+            <p class="lead">Find out where you can give food to organisations that need it. Left over buffet food, or non-perishable unopened items, there are places where it can go! Or perhaps you want to give your time to one of these organisations or find out where and how they can help you.</p>
+            <p class="lead">
+              <a href="map.php" class="btn btn-lg btn-default">View the map</a>
+            </p>
+          </div>
+
+          <div class="mastfoot">
+            <div class="inner">
+              <p>Website by <a href="http://twitter.com/gregorymarler">Gregory Marler</a> of <a href="http://www.nomoregrapes.com">No More Grapes</a>.</p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+
+
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js" />
+</body>
+</html>
