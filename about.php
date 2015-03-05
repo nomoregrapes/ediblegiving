cfef10d6d0b25123ddb00ce7eaff143b74e23b4b
Gregory <info@nomoregrapes.com>
2015-03-05 15:40:07 +0000

    A real website needs an about page.

---
+++ about.php
@@ -0,0 +1,66 @@
+<html>
+<head>
+	<title>About, Edible Giving .org</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" />
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css" />
+
+
+</head>
+
+<body>
+
+    <nav class="navbar navbar-inverse navbar-static-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="/">Edible Giving .org</a>
+			<ul class="nav navbar-nav">
+				<li><a href="map.php">Map</a></li>
+				<li class="active"><a href="about.php">About</a></li>
+				<li><a href="data.php">Add Locations</a></li>
+			</ul>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+        </div><!--/.navbar-collapse -->
+    </nav>
+
+
+	<div class="container">
+		<div class="page-header">
+			<h1>About</h1>
+		</div>
+		<p class="lead">I like food, it is yummy. I am privileged and sometimes I have food left over or can add an extra item to my shopping trolley without any qualms.</p>
+		<p>Can that food go to local people who need it, where and when can I donate the food? I know a few local charities, but if I'm away from home where do I give the food, and what if local details change? I wanted to solve this by creating a UK-wide or international map, and I wanted different organisations to be able to use their own systems to keep the shared map updated. Finally in March 2015 I made a start to this and created Edible Giving, we shall see where it goes.</p>
+		<p></p>
+		<!--
+		I want to write about: Github, 
+		features planned?, 
+		reusing the data (public domain but nice to attribute?),
+		created by me, who?
+		 -->
+	</div> <!-- /container -->
+
+      <hr>
+
+      <footer>
+        <p>Website by <a href="http://twitter.com/gregorymarler">Gregory Marler</a> of <a href="http://www.nomoregrapes.com">No More Grapes</a>.</p>
+      </footer>
+
+
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js" />
+
+</body>
+</html>
