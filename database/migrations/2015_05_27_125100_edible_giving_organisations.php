edd7d6fa37c06c6615daae12139de94452001b04
Gregory <info@nomoregrapes.com>
2015-05-27 15:40:12 +0100

    Add organisation table to db.

---
+++ database/migrations/2015_05_27_125100_edible_giving_organisations.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class EdibleGivingOrganisations extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('organisations', function(Blueprint $table)
+		{
+            $table->increments('id');
+			$table->string('name');
+			$table->string('slug')->index()->unique();
+			$table->string('description')->nullable();
+			$table->string('admin_note')->nullable();
+            $table->timestamps();
+		});
+
+		Schema::table('role_user', function($table)
+		{
+			$table->integer('organisation_id')->unsigned();
+            $table->foreign('organisation_id')->references('id')->on('organisations')
+                ->onUpdate('cascade')->onDelete('cascade');
+
+			//$table->dropPrimary('role_user_user_id_role_id_primary');
+			//$table->primary(['user_id', 'role_id', 'organisation_id']);
+		});
+		DB::unprepared('ALTER TABLE `role_user` DROP PRIMARY KEY, ADD PRIMARY KEY (`user_id`, `role_id`, `organisation_id` )');
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		DB::unprepared('ALTER TABLE `role_user` DROP PRIMARY KEY');
+		Schema::table('role_user', function($table)
+		{
+            //$table->dropPrimary('role_user_id_primary');
+            $table->primary(['user_id', 'role_id']);
+			$table->dropColumn('organisation_id');
+		});
+
+		Schema::drop('organisations');
+	}
+
+}
