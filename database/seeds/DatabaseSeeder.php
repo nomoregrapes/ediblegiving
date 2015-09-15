594d29e568907d385606a1722377caf920ee5314
Gregory <info@nomoregrapes.com>
2015-09-15 15:12:08 +0100

    DB seeder should no longer be used.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,8 +14,8 @@ class DatabaseSeeder extends Seeder {
 	{
 		Model::unguard();
 
-		$this->call('OrganisationTableSeeder');
-		$this->call('UserTableSeeder');
+		//$this->call('OrganisationTableSeeder');
+		//$this->call('UserTableSeeder');
 	}
 
 }
