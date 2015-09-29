618f4d0eecd58f3b48d94ca3677fb0aca3e7e963
Gregory <info@nomoregrapes.com>
2015-09-29 15:11:08 +0100

    Design/content for manage.welcome (new login)

--- resources/views/manage/welcome.blade.php
+++ resources/views/manage/welcome.blade.php
@@ -1,15 +1,24 @@
-@extends('layouts.power')
+@extends('layouts.manage')
 
 @section('title')
 	Management
 @endsection
 
 @section('content')
-	<h1 class="cover-heading">Management index</h1>
+	<h1 class="cover-heading">Management Area</h1>
 	<p class="lead">
-		Welcome, {!! Auth::user()->name_first; !!}!
+		Welcome, {!! Auth::user()->name_first; !!}! To manage locations on Edible Giving, you need to be authorised by an organisation
 	</p>
+	{{-- orgs can authorise people yet.
 	<p>
-		We should tell you instructions on what to do next. You need to be part of an organisation.
+		If a charity or organisation has told you to login, contact them directly as they will now be able to give you access to the management area.
 	</p>
+	--}}
+
+	<p>
+		Are you the lead person in your organisation that will be using Edible Giving? Contact the Edible Giving administrators to give you access to the management area.
+		We'll help you with adding locations where donations can be made, and keeping all infomation up-to-date.
+	</p>
+
+
 @endsection
