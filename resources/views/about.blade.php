960a62500e766808efefd34850a9af93424f664e
Gregory <info@nomoregrapes.com>
2015-10-08 15:37:43 +0100

    More about page content.

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,7 +1,7 @@
-@extends('layouts.master')
+@extends('layouts.manage')
 
 @section('title')
-	About
+	About Edible Giving
 @endsection
 
 @section('content')
@@ -10,8 +10,23 @@
 		<h1>@yield('title')</h1>
 	</div>
 	
-	<p class="lead">I like food, it is yummy. I am privileged and sometimes I have food left over or can add an extra item to my shopping trolley without any qualms.</p>
-	<p>Can that food go to local people who need it, where and when can I donate the food? I know a few local charities, but if I'm away from home where do I give the food, and what if local details change? I wanted to solve this by creating a UK-wide or international map, and I wanted different organisations to be able to use their own systems to keep the shared map updated. Finally in March 2015 I made a start to this and created Edible Giving, we shall see where it goes.</p>
+	<p class="lead">Edible Giving helps you find organisations that you can donate food or your time to. Where you can donate or receive food is shown boldly on the map, and clicking a marker will bring up specific information about that place.</p>
+
+
+	<h2>Information on the website</h2>
+	<p>The information is managed by people within the organisations that are making use of donated food, this keeps it up-to-date and accurate. In some cases, locations have been added by Edible Giving volunteers, so that they are not missed from the website.
+	If you are responsible for an organisation that is missing from Edible Giving, please get in touch so we can help you use the website.</p>
+
+	<h2>Updates</h2>
+	<p>If you want to stay informed about improvements, follow <a href="https://twitter.com/ediblegiving">@EdibleGiving</a> for updates on twitter.</p>
+
+	<h2>Contact</h2>
+	<p>If you have questions, or wish to get in touch then e-mail <em>info</em><em>@ediblegiving</em><em>.org</em>.</p>
+
+	<h2>The History and Vision</h2>
+	<p>I like food, it is yummy. I am privileged and sometimes I have food left over or can add an extra item to my shopping trolley without any qualms.</p>
+	<p>Can that food go to local people who need it, where and when can I donate the food? I know a few local charities, but if I'm away from home where do I give the food, and what if local details change? I wanted to solve this by creating a UK-wide or international map, and I wanted different organisations to be able to use their own systems to keep the shared map updated. Finally in March 2015 I made a start to this and created Edible Giving, we shall see where it goes.
+	The website was created by Gregory Marler.</p>
 	<p></p>
 	<!--
 	I want to write about: Github, 
@@ -19,4 +34,6 @@
 	reusing the data (public domain but nice to attribute?),
 	created by me, who?
 	-->
+
+
 @endsection
